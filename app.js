e3a95dc6c5245353b3838042d37a0d8510e802b6
Theis <thornjakobsen@gmail.com>
2017-01-14 22:09:42 +0100

    added new text and image when clicking a category

--- app.js
+++ app.js
@@ -11,12 +11,47 @@ function loadSummary(userCat) {
         });
         let names = ['Your results', 'Average'];
         data = [userCat, accCat[0]];
-        document.getElementById('userResult').innerHTML = userCat.result.result;
-        document.getElementById('averageResult').innerHTML = accCat[0].result.result;
+
+
+        addResultText(userCat, accCat[0]);
+        addResultImage(userCat);
         loadBarChart(data, "#barchart");
+
+
     });
 
 };
+
+function addResultText(userResults, averageResults) {
+    /*    document.getElementById('userResult').innerHTML = userCat.result.result;
+        document.getElementById('averageResult').innerHTML = accCat[0].result.result;*/
+    var userResultText = userResults.result.result;
+    var averageResultText = averageResults.result.result;
+
+    var outputText = resultTextGenerator(userResultText, averageResultText);
+    var resultHeader = d3.select("p.resultHeader span");
+    resultHeader.html(outputText);
+    console.log(userResults.product.img_path + "large.png")
+}
+
+function resultTextGenerator(string1, string2) {
+    var string = '';
+    if (string1 == string2) {
+        string = "You have <strong>" + string1 + "</strong>, which is also what most people have."
+    } else {
+        string = "You have <strong>" + string1 + "</strong>, while most people got <strong>" + string2 + "</strong>.";
+    }
+    return string;
+}
+
+function addResultImage(userResults) {
+    d3.select("#imageContainer img").remove();
+    var image = d3.select("#imageContainer")
+        .append("img")
+        .attr("src", userResults.product.img_path + "large.png")
+        .attr('height', '100%')
+        .attr('width', '100%');
+}
 
 drawChart("");
 loadSelection(selection);
